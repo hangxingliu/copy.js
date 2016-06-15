8eba52fbd5b00569cd9aedcb02dada477b88a3bf
LiuYue <hangxingliu@gmail.com>
2016-06-15 22:07:57 +0800

    add a dev branch

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/colors/index.d.ts" />
+/// <reference path="globals/fs-extra/index.d.ts" />
+/// <reference path="globals/minimatch/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/watch/index.d.ts" />
